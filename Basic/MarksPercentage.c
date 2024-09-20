f69906528dd940a34bcc96e5228ab6ea962c2fb2
Decoder007 <shashimahrania07@gmail.com>
2024-09-20 21:01:26 +0530

    Create MarksPercentage.c
    coed to calculate percentage of marks of  a student

---
+++ Basic/MarksPercentage.c
@@ -0,0 +1,15 @@
+// code to calculete percentage of students's marks //
+// code by shashi //
+
+#include<stdio.h>
+int main(){
+    float subject1 = 92;
+    float subject2 = 95;
+    float subject3 = 96;
+    float subject4 = 90;
+    float subject5 = 99;
+    float percentage = ((subject1 + subject2 + subject3 + subject4 + subject5) / 500) * 100;
+    printf(" percentage of student is %f " , percentage);
+    return 0;
+
+}
