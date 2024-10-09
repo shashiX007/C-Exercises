8d54dbb415b0b5ba6b23fd9e957e81069fbda6d7
Decoder007 <shashimahrania07@gmail.com>
2024-10-10 01:00:33 +0530

    Update selectionSort.c

--- Sorting algorithms/selectionSort.c
+++ Sorting algorithms/selectionSort.c
@@ -4,10 +4,10 @@ int main() {
     int arr[10] = {9, 2, 3, 1, 7, 6, 4, 8, 5, 0};
 
     // Selection sort algorithm
-    for (int i = 0; i < 10; i++) {//initializing the loop
+    for (int i = 0; i < 10; i++) {
         int minIndex = i;//assuming the index of the min number 
-        for (int j = i + 1; j < 10; j++) {//intialiasing another loop
-            if (arr[j] < arr[minIndex]) {//cmparing the numbers
+        for (int j = i + 1; j < 10; j++) {
+            if (arr[j] < arr[minIndex]) {
                 minIndex = j; // Update minIndex if a smaller element is found
             }
         }
@@ -19,9 +19,9 @@ int main() {
 
     // Print the sorted array
     for (int i = 0; i < 10; i++) {
-        printf("%d ", arr[i]); // Print the value instead of the address
+        printf("%d ", arr[i]);
     }
-    printf("\n"); // New line for better formatting
+    printf("\n");
 
     return 0;
 }
